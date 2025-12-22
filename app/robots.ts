5a6582d9505373d31010714af5271903ba5a1874
naipunyalabs <itinfraadmin@cognitbotz.com>
2025-12-22 16:51:32 +0530

    patch

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next"
 import { config } from "@/lib/config"
 
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = config.baseUrl
 
